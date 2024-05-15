ecf48139cc4fc1e32c4e2f248ba91bd772bd2dd5
syedusman.faisal28 <syedusman.faisal28@gmail.com>
2024-05-15 17:05:03 +0000

    Your commit message here

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,44 @@
 import './style.css'
-function Add(){
-  
-}
+import { useState } from 'react'
 function App() {
+const [newItem, setNewItem] = useState("")
+const [todo, setTodo] = useState([]);
+function handleSubmit(e) {
+  e.preventDefault()
+  setTodo((currenttodo) => { return [...currenttodo,
+     { id: crypto.randomUUID(),
+       title: newItem, completed: false}]  })
+     
+      } 
+      console.log(todo) 
+  
+      
+     
 
 
   return (
     <>
-    <form className="form">
+    <form className="form" onSubmit={handleSubmit}>
       <h1>To do List</h1>
-      <input type="text" placeholder="Add a task" />
+      <input type="text" placeholder="Add a task" value = {newItem} onChange={event => {setNewItem(event.target.value)}} />
       <button type="submit">Add</button>
     </form>
     <h1 className='task-title'>Tasks</h1>
     <ul className="task-list">
-      <li>
-        <input type="checkbox" />
-        <span>Task 1</span>
-        <button>Delete</button>
-      </li>
-      <li>
-        <input type="checkbox" />
-        <span>Task 2</span>
-        <button>Delete</button>
-      </li>
+      {todo.map(item => { return(
+          <li key = {item.id}>
+          <input type="checkbox" />
+          <span>{item.title}</span>
+          <button onClick={e => {
+            setTodo(todo.filter(i => i.id !== item.id))
+          }}>Delete</button>
+        </li>
+
+      )})}
+    
+     
+
+   
     </ul>
     
     </>
